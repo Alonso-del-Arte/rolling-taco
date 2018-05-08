156555ee4da7f8ff2d8520747483f93d8a8060fa
Alonso del Arte <alonso.delarte@gmail.com>
2018-05-08 02:27:34 -0400

    Update and rename tests/TacoTruckWelcomePanelTest.java to tests/view/TacoTruckWelcomePanelTest.java
    Moved to package "view"

--- tests/view/TacoTruckWelcomePanelTest.java
+++ tests/view/TacoTruckWelcomePanelTest.java
@@ -1,4 +1,9 @@
+package view;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import view.TacoTruckWelcomePanel;
 
 import javax.swing.*;
 
@@ -10,11 +15,12 @@ public class TacoTruckWelcomePanelTest {
     public void welcomePanel_initialState() {
 
         TacoTruckWelcomePanel welcome = new TacoTruckWelcomePanel();
-        JTextField customerNameField = (JTextField) welcome.getComponents() [0];
-        JButton startButton = (JButton) welcome.getComponents() [1];
+        JTextField customerNameField = (JTextField) welcome.getComponents() [1];
+        JButton startButton = (JButton) welcome.getComponents() [2];
+        JLabel customerPrompt = (JLabel) welcome.getComponents()[0];
 
 
-        assertEquals("TacoTruckWelcomePanel", welcome.getName());
+        assertEquals("view.TacoTruckWelcomePanel", welcome.getName());
 
         assertEquals("StartOrderButton",welcome.getStartButton().getName());
         assertEquals("Start Order",welcome.getStartButton().getText());
@@ -23,16 +29,11 @@ public class TacoTruckWelcomePanelTest {
         assertEquals(startButton, welcome.getStartButton());
 
         assertEquals("CustomerNameField", welcome.getCustomerNameField().getName());
-        assertEquals("Enter Customer Name", welcome.getCustomerNameField().getText());
+        assertEquals("", welcome.getCustomerNameField().getText());
         assertEquals("CustomerNameField", customerNameField.getName());
-        assertEquals("Enter Customer Name", customerNameField.getText());
+        assertEquals("", customerNameField.getText());
         assertEquals(customerNameField, welcome.getCustomerNameField());
 
     }
-
-
-
-
-
 
 }
