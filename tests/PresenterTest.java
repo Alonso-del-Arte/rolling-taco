4d7d4700627d3ce1ed99e1903b5eebbc1c0874f6
Alonso del Arte <alonso.delarte@gmail.com>
2018-05-08 02:30:51 -0400

    Update PresenterTest.java

--- tests/PresenterTest.java
+++ tests/PresenterTest.java
@@ -1,55 +1,30 @@
+import model.TacoTruck;
 import org.junit.Before;
-
 import org.junit.Test;
-
-
-
+import presenter.Presenter;
+import view.TacoTruckWelcomePanel;
 import javax.swing.*;
-
-
-
 import java.awt.*;
-
-
-
-import static org.junit.Assert.*;
-
-
+mport static org.junit.Assert.*;
 
 public class PresenterTest {
 
-
-
     Presenter presenter;
 
-
-
     @Before
-
     public void setUp() {
-
         TacoTruck view = new TacoTruck();
-
         presenter = new Presenter(view);
-
     }
 
-
-
     @Test
-
     public void testPresenterInitializesWithStartScree() {
 
         TacoTruck view = new TacoTruck();
-
         Presenter presenter = new Presenter(view);
-
         Container currentScreen = presenter.getCurrentScreen();
-
         assertEquals(view, presenter.getView());
-
-        assertEquals("TacoTruckWelcomePanel", currentScreen.getName());
-
+        assertEquals("view.TacoTruckWelcomePanel", currentScreen.getName());
         assertEquals(currentScreen, presenter.getView().getContentPane());
 
     }
@@ -61,13 +36,8 @@ public class PresenterTest {
     public void testStartButtonOnClickGoesToOrderScreen () {
 
         JButton startButton = ((TacoTruckWelcomePanel) presenter.getCurrentScreen()).getStartButton();
-
         startButton.doClick();
-
         assertEquals("TacoTruckOrderPanel", presenter.getCurrentScreen().getName());
-
     }
-
-
 
 }
